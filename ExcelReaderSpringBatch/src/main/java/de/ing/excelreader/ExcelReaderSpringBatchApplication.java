0a05d0c4de21fb5499c5cbaad343bfeb4bdeee07
limagoHub <limagoHub@limago.de>
2020-02-23 11:12:21 +0100

    Initial commit

---
+++ ExcelReaderSpringBatch/src/main/java/de/ing/excelreader/ExcelReaderSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package de.ing.excelreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelReaderSpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelReaderSpringBatchApplication.class, args);
+	}
+
+}
---
+++ ExcelReaderSpringBatch/src/test/java/de/ing/excelreader/ExcelReaderSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ing.excelreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelReaderSpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
