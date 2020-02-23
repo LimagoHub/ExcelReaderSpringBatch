8c2bc5cf0316e7723cd5c9eedf0e77022af1eb40
limagoHub <limagoHub@limago.de>
2020-02-23 14:48:51 +0100

    Simple Batch Demo inserted

--- ExcelReaderSpringBatch/src/main/java/de/ing/excelreader/ExcelReaderSpringBatchApplication.java
+++ ExcelReaderSpringBatch/src/main/java/de/ing/excelreader/ExcelReaderSpringBatchApplication.java
@@ -1,13 +1,48 @@
 package de.ing.excelreader;
 
+import java.util.UUID;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class ExcelReaderSpringBatchApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ExcelReaderSpringBatchApplication.class, args);
-	}
+		ApplicationContext context = SpringApplication.run(ExcelReaderSpringBatchApplication.class, args);
+		
+		JobLauncher jobLauncher = context.getBean(JobLauncher.class);
+        Job job = context.getBean("listEmployeesJob", Job.class);
+
+        JobParameters jobParameters = new JobParametersBuilder().addString("ID", UUID.randomUUID().toString()).toJobParameters();
+    
+        try {
+            JobExecution jobExecution = jobLauncher.run(job, jobParameters);
+        }
+        catch (JobExecutionAlreadyRunningException e) {
+            e.printStackTrace();
+        }
+        catch (JobRestartException e) {
+            e.printStackTrace();
+        }
+        catch (JobInstanceAlreadyCompleteException e) {
+            e.printStackTrace();
+        }
+        catch (JobParametersInvalidException e) {
+            e.printStackTrace();
+        }
+    
+    }
+	
 
 }
---
+++ ExcelReaderSpringBatch/src/main/java/de/ing/excelreader/batch/SpringBatchHelloWorldConfig.java
@@ -0,0 +1,97 @@
+package de.ing.excelreader.batch;
+
+import java.util.List;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+@Configuration
+@EnableBatchProcessing
+public class SpringBatchHelloWorldConfig {
+
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .<Employee, Employee>chunk(2)
+                .reader(employeeItemReader())
+                .processor(employeeItemProcessor())
+                .writer(employeeItemWriter())
+                .build();
+    }
+
+    @Bean
+    public Job listEmployeesJob(Step step1) throws Exception {
+        return jobBuilderFactory.get("listEmployeesJob").incrementer(new RunIdIncrementer())
+                .start(step1)
+                .build();
+    }
+
+    @Bean
+    ItemReader<Employee> employeeItemReader() {
+        
+        FlatFileItemReader<Employee> reader = new FlatFileItemReader<>();
+        reader.setResource(new ClassPathResource("employees.csv"));
+
+        DefaultLineMapper defaultLineMapper = new DefaultLineMapper();
+        DelimitedLineTokenizer delimitedLineTokenizer = new DelimitedLineTokenizer();
+        delimitedLineTokenizer.setNames(new String[] {"firstName", "lastName", "age", "salary"});
+
+        BeanWrapperFieldSetMapper<Employee> fieldSetMapper = new BeanWrapperFieldSetMapper<>();
+        fieldSetMapper.setTargetType(Employee.class);
+
+        defaultLineMapper.setLineTokenizer(delimitedLineTokenizer);
+        defaultLineMapper.setFieldSetMapper(fieldSetMapper);
+        reader.setLineMapper(defaultLineMapper);
+
+        return reader;
+    }
+
+    @Bean
+    ItemProcessor<Employee, Employee> employeeItemProcessor() {
+        return new ItemProcessor<Employee, Employee>() {
+            @Override
+            public Employee process(Employee employee) throws Exception {
+                employee.setFirstName(employee.getFirstName().toUpperCase());
+                employee.setLastName(employee.getLastName().toUpperCase());
+                return employee;
+            }
+        };
+    }
+    
+    @Bean
+    ItemWriter<Employee> employeeItemWriter() {
+        return new ItemWriter<Employee>() {
+            @Override
+            public void write(List<? extends Employee> employeesList) throws Exception {
+                for (Employee employee : employeesList) {
+                    System.out.println("Name: "
+                            + employee.getFirstName() + " "
+                            + employee.getLastName() + "; "
+                            + "Age: " + employee.getAge() + "; "
+                            + "Salary: " + employee.getSalary());
+                }
+            }
+        };
+    }
+}
