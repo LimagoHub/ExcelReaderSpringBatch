c453032075f9843d838671dd1d83b31aee508203
limagoHub <limagoHub@limago.de>
2020-04-22 09:36:18 +0200

    start mittwoch

--- ExcelReaderSpringBatch/src/main/java/de/ing/excelreader/batch/Employee.java
+++ ExcelReaderSpringBatch/src/main/java/de/ing/excelreader/batch/Employee.java
@@ -1,9 +1,12 @@
 package de.ing.excelreader.batch;
 
-import org.springframework.batch.core.annotation.AfterChunk;
-import org.springframework.batch.item.ResourceAware;
-import org.springframework.core.io.Resource;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Employee {
     private String firstName;
     private String lastName;
@@ -11,47 +14,5 @@ public class Employee {
     private int salary;
     
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public int getSalary() {
-        return salary;
-    }
-
-    public void setSalary(int salary) {
-        this.salary = salary;
-    }
-
-	@Override
-	public String toString() {
-		return "Employee [firstName=" + firstName + ", lastName=" + lastName + ", age=" + age + ", salary=" + salary
-				+ "]";
-	}
-    
-    
-
-		
 	
 }
